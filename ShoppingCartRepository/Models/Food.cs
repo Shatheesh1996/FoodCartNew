66f7f64241c0d89c1dd859ffbd429ae9c9378e8b
shatheesh.A <shatheesh.A@SHATHEESH>
2022-10-29 17:02:48 +0530

    Add project files.

---
+++ ShoppingCartRepository/Models/Food.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCartRepository.Models
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Instock { get; set; }
+        public string imageurl { get; set; }
+    }
+}
---
+++ ShoppingCartRepository/Processor/ShoppingCartProcessor.cs
@@ -0,0 +1,34 @@
+﻿using ShoppingCartRepository.Interface;
+using ShoppingCartRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ShoppingCartRepository.Processor
+{
+    public class ShoppingCartProcessor : IShoppingCart
+    {
+        public Food GetShoppingCartItem(int id)
+        {
+            Connection con = new Connection();
+            SqlConnection Get_connection = new SqlConnection(con.GetConnection());
+            SqlDataAdapter Get_adapter = new SqlDataAdapter("select name,Description,price,Instock,imageurl from food where FC_id = (select food_id from shoppingcart_item where userid = '" + id + "' )", Get_connection);
+            DataTable get_table = new DataTable();
+            Get_adapter.Fill(get_table);
+
+            Food Getprop_obj = new Food()
+            {
+                Name = Convert.ToString(get_table.Rows[0]["name"]),
+                Description = Convert.ToString(get_table.Rows[0]["name"]),
+                Price = Convert.ToInt32(get_table.Rows[0]["price"]),
+                Instock = Convert.ToInt32(get_table.Rows[0]["instock"]),
+                imageurl = Convert.ToString(get_table.Rows[0]["imageurl"])
+            };
+
+            return Getprop_obj;
+
+        }
+    }
+}
